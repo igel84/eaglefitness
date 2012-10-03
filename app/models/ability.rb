a71c9ccc7a6cc5eaae80d1c080914caab6cca9be
danila <kdanilke@gmail.com>
2012-10-03 08:02:37 +0200

    check commit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,7 +9,8 @@ class Ability
     @user.roles.each { |role| send(role) }
 
     if @user.roles.size == 0
-      can :read, :all #for guest without roles
+      can :read, Event, Pages, Coaches, Categories
+      
     end
   end
 
